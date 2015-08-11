e8ff2d6b5e856ed2c784a9b62a984dfbe9c28ab2
yinaura <yinaura@aiming-inc.com>
2015-08-11 18:25:15 +0900

    Deviseの設定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  resources :user_profiles, only: [:show, :edit, :update]
+  devise_for :users, controllers: {
+    omniauth_callbacks: "users/omniauth_callbacks"
+  }
+  resources :user_profiles, only: [:show, :new, :create, :edit, :update]
 end
