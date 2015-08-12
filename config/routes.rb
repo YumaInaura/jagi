9fa3c7d7ce21d090095bf3137eeea46e6f497bd5
yinaura <yinaura@aiming-inc.com>
2015-08-12 10:00:16 +0900

    トップページをルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
-  resources :user_profiles, only: [:show, :new, :create, :edit, :update]
+  resources :page, only: [:index, :show, :new, :create, :edit, :update]
+  resources :user_profiles, only: [:index, :show, :new, :create, :edit, :update]
+  root to: "page#index"
 end
