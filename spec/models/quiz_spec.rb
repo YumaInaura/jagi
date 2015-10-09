a135802fc14d4380cc8dc8300e013b771d6c8947
yinaura <yinaura@aiming-inc.com>
2015-10-09 18:23:18 +0900

    refactor test on Quiz model

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -2,76 +2,85 @@ require 'rails_helper'
 
 RSpec.describe Quiz, type: :model do
   describe '.correct?' do
-    describe 'Standard name' do
+    context 'When correct answer is English' do
       let(:question_user_profile) { FactoryGirl.create :user_profile }
 
-      describe 'is matched' do
-        it 'at answer name' do
+      describe 'Answer matches' do
+        example 'to free style correct answer' do
           expect(Quiz.correct?(question_user_profile, question_user_profile.user.name)).to be true
         end
 
-        it 'at Google full name' do
+        example 'with full name' do
           expect(Quiz.correct?(question_user_profile, question_user_profile.answer_name)).to be true
         end
 
-        it 'at last name' do
+        example 'with last name' do
           expect(Quiz.correct?(question_user_profile, question_user_profile.user.name[0..1])).to be true
         end
 
-        it 'at first name' do
+        example 'with first name' do
           expect(Quiz.correct?(question_user_profile, question_user_profile.user.name[-2..-1])).to be true
+        end
+      end
+
+      describe 'Answer does not matches' do
+        example 'with empty text' do
+          expect(Quiz.correct?(question_user_profile, '')).to be false
+        end
+
+        example 'with space all text' do
+          expect(Quiz.correct?(question_user_profile, ' ')).to be false
         end
       end
     end
 
-    describe 'Japanese name' do
+    context 'When correct answer is Japanese' do
       let(:question_user) { FactoryGirl.create :user, :with_user_profile, name: '半沢直樹' }
 
-      describe 'is matched' do
-        describe 'on hiragana' do
-          it 'at full name ' do
+      describe 'Hiragana answer' do
+        describe 'Matches' do
+          example 'with full name ' do
             expect(Quiz.correct?(question_user.user_profile, 'はんざわなおき')).to be true
           end
 
-          it 'at full name with spece' do
+          example 'with has space full name' do
             expect(Quiz.correct?(question_user.user_profile, 'はんざわ なおき')).to be true
           end
 
-          it 'at last name' do
+          example 'with last name' do
             expect(Quiz.correct?(question_user.user_profile, 'はんざわ')).to be true
           end
 
-          it 'at first name' do
+          example 'with first name' do
             expect(Quiz.correct?(question_user.user_profile, 'なおき')).to be true
           end
         end
 
-        describe 'on katakana' do
-          it 'at full name ' do
-            expect(Quiz.correct?(question_user.user_profile, 'ハンザワナオキ')).to be true
+        describe 'Does not matches' do
+          example 'with not enough length name.' do
+            expect(Quiz.correct?(question_user.user_profile, 'はん')).to be false
+            expect(Quiz.correct?(question_user.user_profile, 'おき')).to be false
           end
 
-          it 'at last name' do
-            expect(Quiz.correct?(question_user.user_profile, 'ハンザワ')).to be true
-          end
-
-          it 'at first name' do
-            expect(Quiz.correct?(question_user.user_profile, 'ナオキ')).to be true
+          example 'with imcompleted name.' do
+            expect(Quiz.correct?(question_user.user_profile, 'んざわなお')).to be false
           end
         end
       end
 
-      describe 'is not matched' do
-        it 'at last name' do
-          expect(Quiz.correct?(question_user.user_profile, 'はん')).to be false
-        end
+      describe 'Katakana answer' do
+        describe 'Matches' do
+          example 'with full name ' do
+            expect(Quiz.correct?(question_user.user_profile, 'ハンザワナオキ')).to be true
+          end
 
-        it 'at first name' do
-          expect(Quiz.correct?(question_user.user_profile, 'おき')).to be false
-        end
+          example 'with last name' do
+            expect(Quiz.correct?(question_user.user_profile, 'ハンザワ')).to be true
+          end
 
-        it 'at imcompleted name' do
-          expect(Quiz.correct?(question_user.user_profile, 'んざわなお')).to be false
+          example 'with first name' do
+            expect(Quiz.correct?(question_user.user_profile, 'ナオキ')).to be true
+          end
         end
       end
     end
